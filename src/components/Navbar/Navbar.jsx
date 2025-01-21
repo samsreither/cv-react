5ee5b30cd2306d4a01c7dbedb49362438d9fd40f
Sam Reither <samsreither@gmail.com>
2025-01-21 13:09:17 -0500

    working on the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,42 @@
-import React, {useState} from 'react'
-import styles from './Navbar.module.css'
-import { getImageUrl } from '../../utils'
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-        <a className={styles.title} href="/">Portfolio</a>
-        <div className={styles.menu}>
-            <img src={getImageUrl("nav/menuIcon.png")} alt="menu-button" className={styles.menuBtn} />
-            <ul className={styles.menuItems}>å
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </div>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          className={styles.menuBtn}
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        {/* add menuOpen class when the three lines are clicked */}
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
+          å
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
